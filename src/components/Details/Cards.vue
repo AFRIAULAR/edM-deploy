340e44185e51eab11491a4ee1eccbd2a319f0cd8
AFRIAULAR <aularafrica@gmail.com>
2023-12-10 14:18:08 -0300

    Adding dynamic cards

--- src/components/Details/Cards.vue
+++ src/components/Details/Cards.vue
@@ -1,109 +1,72 @@
 <template>
-  <div class="container" style="max-height: 111px; overflow-y: auto;">
-    <div class="d-flex flex-wrap">
-      <div v-for="(card, index) in cards" :key="index" class="col-md-4 col-lg-3 mb-4">
-        <div class="card-content">
-          <div class="card-body-content">
-            <div class="d-flex justify-content-between align-items-center mb-3">
-              <div class="icon-background">
-                <div class="material-icon">{{ card.icon }}</div>
-              </div>
-              <div class="text-right">
-                <div class="font-weight-bold">{{ card.title }}</div>
-                <div>{{ card.subtitle }}</div>
-              </div>
-            </div>
-            <div class="d-flex justify-content-between align-items-start">
-              <div class="text-left">
-                <div class="font-weight-bold">Created on</div>
-                <div>{{ card.created }}</div>
-              </div>
-              <div class="text-right">
-                <div class="font-weight-bold">{{ card.label }}</div>
-                <div>{{ card.updated }}</div>
-              </div>
-            </div>
-          </div>
+  <div class="d-flex card-container">
+    <div v-for="(item, index) in items" :key="index" class="flex-grow-1 p-1 bg-white rounded  m-2">
+      <div class="card-container-upper d-flex justify-content-between align-items-center mb-1">
+        <div class="card-icon rounded-circle d-flex justify-content-center align-items-center">
+          <span class="text-white material-icons">{{ item.icon }}</span>
         </div>
+        <div class="text-right">
+          <div class="card-text font-weight-bold text-muted">{{ item.title }}
+          <span
+            v-if="item.icon2"
+            class="material-icons icon2">{{ item.icon2 }}</span>
+          </div>
+          <div class="subtitle">{{ item.subtitle }} </div>
+        </div>
+      </div>
+      <div class="card-container-lower d-flex justify-content-between align-items-start">
+        <div class="card-text text-muted ">{{ item.label1 }}</div>
+        <div class="card-text text-muted">{{ item.label2 }}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import itemsData from '../../utils/itemsData.json';
 export default {
   data() {
     return {
-      cards: [
-        {
-          icon: 'today',
-          title: 'Record Age',
-          subtitle: '12 days',
-          created: 'Jan 2, 2023',
-          label: 'Updated',
-          updated: 'Nov 2, 2023'
-        },
-        {
-          icon: 'check_circle',
-          title: 'Status',
-          subtitle: 'Active',
-          label: 'Updated',
-          updated: 'Nov 2, 2023'
-        },
-        {
-          icon: 'location_on',
-          title: 'Country',
-          subtitle: 'México',
-          label: 'State',
-          updated: 'Córdoba'
-        },
-        {
-          icon: 'contact_page',
-          title: 'Last Contact',
-          subtitle: '2 days ago',
-          label: 'Nov 29, 2023',
-          updated: '2:43pm'
-        },
-        {
-          icon: 'timeline',
-          title: 'Last Activity',
-          subtitle: '7 days ago',
-          label: 'Nov 29, 2023',
-          updated: '2:43pm'
-        }
-      ]
+      items:itemsData
     };
   }
 };
 </script>
 
 <style>
-.icon-background {
+.material-icons {
+  font-size: 25px;
+}
+.card-container{
+ margin:10px
+}
+.card-icon{
+  background-color: #91cc75;
   width: 40px;
   height: 40px;
-  background: #91CC75;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
-
-.material-icon {
-  color: white;
-  font-size: 25px;
-  font-family: 'Material Icons';
-  font-weight: 400;
+.subtitle{
+  font-size: 18px;
+  color: #6b6b7b
 }
-.card-content{
-  width: 207px;
-  height: 111px;
-  border-radius: 6%;
-  padding: 15px;
-  margin: 10px;
-} 
-.card-body-content {
+.card-text{
+  font-size: 14px;
+}
+.card-container-upper{
+  border-bottom: 1px solid #dddddd;
+  padding: 10px;
+  color: #6b7e6b;
   display: flex;
-  flex-direction: column;
-  font-size: 14px; 
+  font-weight: 700;
+  justify-content: space-between;
+}
+.card-container-lower{
+  color: #6b6b7b;
+  padding: 5px;
+  font-weight: 700;
+}
+.icon2{
+  color: #6b6b7b;
+  font-size: 16px;
 }
 </style>
